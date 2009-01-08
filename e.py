a7bba4cf3288652406c13856bbf4370ac7d63681
Matt Gerassimoff <mgeras@gmail.com>
2009-01-08 12:13:46 -0700

    fix project creation/deletion.

--- e.py
+++ e.py
@@ -149,12 +149,10 @@ class Project:
 	value, name = line.split(',')
 	self.slots.append(Slot(self, slot, value, name))
 	slot += 1
-    else:
-      self.slots.append(Slot(self, 0))
 
   def write(self):
     f = open(self.e.home+'/'+self.name+'.project','w')
-    while len(self.slots) > 1 and self.slots[-1].value == '':
+    while self.slots[-1].value == '':
       self.slots.pop(-1)
     for slot in self.slots:
       f.write('%s,%s\n' % (slot.value, slot.name))
@@ -284,7 +282,6 @@ class E:
     if os.path.exists(fname + '.oldproject'):
       os.rename(fname + '.oldproject', fname + '.project')
     self.projects[name] = Project(self, name)
-    self.projects[name].write()
     self.update_eprojects()
 
   def update_eprojects(self):
@@ -432,9 +429,6 @@ class E:
       self.set_current_project(self.projects['default'])
 
     del self.projects[name]
-
-    fname = self.home + '/' + name
-    os.rename(fname + '.project', fname + '.oldproject')
 
     self.update_eprojects()
     self.ls()
